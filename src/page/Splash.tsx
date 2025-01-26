b83af2f9c13c0d4c7bfe32d810010f21637028d5
nemobim <drakequation@naver.com>
2025-01-26 17:15:27 +0900

    [fix] 스플래시 화면 로티로 변경

--- src/page/Splash.tsx
+++ src/page/Splash.tsx
@@ -1,19 +1,13 @@
-import { useEffect } from 'react'
+import Lottie from 'lottie-react'
 import { useNavigate } from 'react-router-dom'
+import splash from '../assets/lottie/splash.json'
 
 const Splash = () => {
   const navigate = useNavigate()
 
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('/tutorials')
-      //2초 후 이동
-    }, 2000)
-  }, [navigate])
-
   return (
     <div className="flex h-full items-center justify-center">
-      <h1 className="text-6xl">이불 킥</h1>
+      <Lottie animationData={splash} loop={false} onComplete={() => navigate('/tutorials')} />
     </div>
   )
 }
