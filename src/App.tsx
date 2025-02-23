bb0d90777f44a163a7c6e3a5042c22047f940c93
nemo-nemo-bim <drakequation@naver.com>
2025-02-24 01:11:05 +0900

    [fix] 화면 사이즈 조정

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,8 @@ const App = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      //넓이 310px * 660px  이하의 화면은 게임 불가
-      setIsSmallScreen(window.innerWidth <= 310 || window.innerHeight <= 660)
-
-      alert(window.innerWidth + ' ' + window.innerHeight)
+      //넓이 310px * 630px  이하의 화면은 게임 불가
+      setIsSmallScreen(window.innerWidth <= 310 || window.innerHeight <= 629)
     }
 
     // 초기 화면 크기 확인
@@ -26,10 +24,8 @@ const App = () => {
   }, [])
 
   if (isSmallScreen) {
-    return <Error title="화면 사이즈 확인!" text="원활한 게임 플레이를 위해 권장 사이즈(310*660) 이상으로 플레이해주세요." />
+    return <Error title="화면 사이즈 확인!" text="원활한 게임 플레이를 위해 권장 사이즈(310*630) 이상으로 플레이해주세요." />
   }
-
-  console.log(navigator.mediaDevices, navigator?.userAgent)
 
   return (
     <div className="min-h-screen bg-gray-100">
--- src/utils/scripts.ts
+++ src/utils/scripts.ts
@@ -6,5 +6,5 @@ export const bedScripts = [
   { img: userPhone, script: '(휴대폰 하는 중...)' },
   { img: userPhone, script: '하... 벌써 세시네 자야겠다.' },
   { img: userSleep, script: 'Z..z..zzz...zz' },
-  { img: userWakeUp, script: '자꾸 그때 생각이나!' },
+  { img: userWakeUp, script: '자꾸 그 생각때매 잠이 안 와!' },
 ]
